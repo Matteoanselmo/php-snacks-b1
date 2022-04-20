0aa85833cf6b0836808ea346eff97d35a4e717ca
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-04-20 12:14:27 +0200

    es-2

---
+++ es-2/index.php
@@ -0,0 +1,23 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sanck 2</title>
+</head>
+<body>
+    <p>
+        <?php
+        $params = $_GET;
+            if((strlen($params['name']) > 3) && (strpos($params['mail'], '@')) && (strpos($params['mail'], '.')) &&(is_numeric($params['age']))){
+                echo $check = 'accesso riuscito';
+            } else{
+                echo $check = 'accesso negato';
+            }
+        ?>
+    </p>
+</body>
+</html>
